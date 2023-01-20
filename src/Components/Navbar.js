ac47f116697df0ae5a356fd22b9b73a69bc5767b
Gopal Adhikari <gopuadks@gmail.com>
2023-01-20 22:48:08 +0545

    Fixed routing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/travel-with-a-cause">
+        <Link className="navbar-brand" to="/travel-with-a-cause/">
           <img
             src="https://static.wixstatic.com/media/30f17f_b93910978db8496490fe69c28bcfefb0~mv2.png/v1/crop/x_0,y_6,w_509,h_304/fill/w_184,h_92,al_c,q_95,enc_auto/twac%20white%20logo.png"
             className="img-fluid logo"
@@ -26,38 +26,47 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" to="/travel-with-a-cause">
+              <Link className="nav-link" to="/travel-with-a-cause/">
                 Home
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/corporate-sustainable">
+              <Link
+                className="nav-link"
+                to="/travel-with-a-cause/corporate-sustainable"
+              >
                 Corporate Sustainable
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/spirit-of-tasmania">
+              <Link
+                className="nav-link"
+                to="/travel-with-a-cause/spirit-of-tasmania"
+              >
                 Spirit of Tasmania
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/pass-park">
+              <Link className="nav-link" to="/travel-with-a-cause/pass-park">
                 Park Pass
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/travel-and-more">
+              <Link
+                className="nav-link"
+                to="/travel-with-a-cause/travel-and-more"
+              >
                 Travel & more
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about-page">
+              <Link className="nav-link" to="/travel-with-a-cause/about-page">
                 About
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link className="nav-link" to="/travel-with-a-cause/contact">
                 Contact
               </Link>
             </li>
